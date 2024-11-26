15dcfb18755f639bf666b6d6a189977bfdd588d7
Булат Хангильдин <khangildinba@gmail.com>
2024-11-26 23:38:29 +0300

    add snippets supprot

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -2,13 +2,14 @@ return {
   "hrsh7th/nvim-cmp",
   commit = "be7bd4c",
   dependencies = {
-    { "L3MON4D3/LuaSnip",         tag = "v2.3.0" },
-    { "hrsh7th/cmp-buffer",       commit = "3022dbc" },
-    { "hrsh7th/cmp-cmdline",      commit = "d250c63" },
-    { "hrsh7th/cmp-nvim-lsp",     commit = "39e2eda" },
-    { "hrsh7th/cmp-path",         commit = "91ff86c" },
-    { "onsails/lspkind.nvim",     commit = "a700f14" },
-    { "saadparwaiz1/cmp_luasnip", commit = "98d9cb5" },
+    { "L3MON4D3/LuaSnip",             tag = "v2.3.0" },
+    { "hrsh7th/cmp-buffer",           commit = "3022dbc" },
+    { "hrsh7th/cmp-cmdline",          commit = "d250c63" },
+    { "hrsh7th/cmp-nvim-lsp",         commit = "39e2eda" },
+    { "hrsh7th/cmp-path",             commit = "91ff86c" },
+    { "onsails/lspkind.nvim",         commit = "a700f14" },
+    { "saadparwaiz1/cmp_luasnip",     commit = "98d9cb5" },
+    { "rafamadriz/friendly-snippets", commit = "de8fce9" },
   },
   config = function()
     vim.opt.completeopt = { "menu", "menuone", "noselect" }
@@ -54,7 +55,6 @@ return {
       history = false,
       updateevents = "TextChanged,TextChangedI",
     }
-
     for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("lua/custom/snippets/*.lua", true)) do
       loadfile(ft_path)()
     end
@@ -70,5 +70,7 @@ return {
         ls.jump(-1)
       end
     end, { silent = true })
+
+    require("luasnip.loaders.from_vscode").lazy_load()
   end,
 }
