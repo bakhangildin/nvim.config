7c5544ed12657264c9873bfe923741d1697c14e3
Булат Хангильдин <hangildin@bo-energo.ru>
2024-12-20 16:21:15 +0000

    switched to tokionight theme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -2,6 +2,7 @@ return {
   {
     "tjdevries/colorbuddy.nvim",
     commit = "8b968581e5c19d22a861d5f3fe5dbd83394fa681",
+    enabled = false,
     config = function()
       vim.cmd.colorscheme "gruvbuddy"
 
@@ -11,6 +12,23 @@ return {
       vim.api.nvim_set_hl(0, "LineNr", { bg = bg })
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#181818" })
       vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { link = "DiagnosticUnnecessary" })
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "storm",
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
+        },
+      })
+      vim.cmd([[colorscheme tokyonight]])
     end,
   },
 }
--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -9,18 +9,18 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup {
-        signs = {
-          add = { text = "+" },
-          change = { text = "~" },
-          delete = { text = "-" },
-          topdelete = { text = "‾" },
-        },
-        signs_staged = {
-          add = { text = "+" },
-          change = { text = "~" },
-          delete = { text = "-" },
-          topdelete = { text = "-" },
-        },
+        -- signs = {
+        --   add = { text = "+" },
+        --   change = { text = "~" },
+        --   delete = { text = "-" },
+        --   topdelete = { text = "‾" },
+        -- },
+        -- signs_staged = {
+        --   add = { text = "+" },
+        --   change = { text = "~" },
+        --   delete = { text = "-" },
+        --   topdelete = { text = "-" },
+        -- },
       }
     end,
   },
