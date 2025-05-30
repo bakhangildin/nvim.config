db604a9e7cbe165ee2c2b90dd59928adfb8845c0
Булат Хангильдин <khangildinba@gmail.com>
2025-05-30 11:46:20 +0300

    js/ts and jsx/tsx opts

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -1,6 +1,3 @@
-vim.opt_local.tabstop = 4
-vim.opt_local.shiftwidth = 4
-
 vim.api.nvim_create_user_command("TSOrganizeImports", function()
   vim.lsp.buf.execute_command({
     command = "_typescript.organizeImports",
--- after/ftplugin/typescriptreact.lua
+++ after/ftplugin/typescriptreact.lua
@@ -1,5 +1,4 @@
-vim.opt_local.tabstop = 4
-vim.opt_local.shiftwidth = 4
+vim.cmd "syn include @html syntax/html.vim"
 
 vim.api.nvim_create_user_command("TSOrganizeImports", function()
   vim.lsp.buf.execute_command({
