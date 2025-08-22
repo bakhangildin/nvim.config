8f032e2cb9e632c474375fb8db1c71bc83d60855
Булат Хангильдин <khangildinba@gmail.com>
2025-08-22 12:17:12 +0300

    update "tpope/vim-fugitive"

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,7 +1,7 @@
 return {
   {
     "tpope/vim-fugitive",
-    commit = "593f831d6f6d779cbabb70a4d1e6b1b1936a88af",
+    commit = "61b51c09b7c9ce04e821f6cf76ea4f6f903e3cf4",
     config = function()
       vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
     end,
