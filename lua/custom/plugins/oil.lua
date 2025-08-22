3fdfc94a6405067cf21133f54704267da7cb74cf
Булат Хангильдин <khangildinba@gmail.com>
2025-08-22 12:02:25 +0300

    update oil.nvim to v2.15.0

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/oil.nvim",
-    tag = "v2.13.0",
+    tag = "v2.15.0",
     dependencies = {
       { "nvim-tree/nvim-web-devicons", commit = "19d6211c78169e78bab372b585b6fb17ad974e82" },
     },
