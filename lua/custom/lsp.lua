20e2d20fddbfd707caede6d39f88399dae1f91e7
Булат Хангильдин <khangildinba@gmail.com>
2025-11-25 10:22:31 +0300

    disable protobuf lsp

--- lua/custom/lsp.lua
+++ lua/custom/lsp.lua
@@ -166,23 +166,23 @@ lspconfig.clangd.setup {
 }
 
 
-local configs = require("lspconfig.configs")
-local util = require("lspconfig.util")
-configs.protobuf_language_server = {
-  default_config = {
-    cmd = { "protobuf-language-server" },
-    filetypes = { "proto", "cpp" },
-    root_dir = util.root_pattern(".git"),
-    single_file_support = true,
-    settings = {
-      -- ["additional-proto-dirs"] = [
-      --     -- path to additional protobuf directories
-      --     -- "vendor",
-      --     -- "third_party",
-      -- ]
-    },
-  }
-}
-lspconfig.protobuf_language_server.setup {
-  capabilities = capabilities,
-}
+-- local configs = require("lspconfig.configs")
+-- local util = require("lspconfig.util")
+-- configs.protobuf_language_server = {
+--   default_config = {
+--     cmd = { "protobuf-language-server" },
+--     filetypes = { "proto", "cpp" },
+--     root_dir = util.root_pattern(".git"),
+--     single_file_support = true,
+--     settings = {
+--       -- ["additional-proto-dirs"] = [
+--       --     -- path to additional protobuf directories
+--       --     -- "vendor",
+--       --     -- "third_party",
+--       -- ]
+--     },
+--   }
+-- }
+-- lspconfig.protobuf_language_server.setup {
+--   capabilities = capabilities,
+-- }
