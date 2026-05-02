8614ed66f9cc3c7510ffd1b31451255b614c6332
Булат Хангильдин <khangildinba@gmail.com>
2026-05-02 20:09:48 +0300

    Revert "jinja lsp"
    This reverts commit dc303588fb84595c7d2591ff7beed9674075bb6f.

--- lua/custom/lsp.lua
+++ lua/custom/lsp.lua
@@ -102,8 +102,6 @@ require("mason-tool-installer").setup {
     { "ols",                         version = "dev-2025-11" },
 
     { "arduino-language-server",     version = "0.7.7" },
-
-    { "jinja-lsp",                   version = "0.1.91" },
   },
   auto_update = false,
 }
@@ -190,9 +188,6 @@ lspconfig.arduino_language_server.setup {
   filetypes = { "arduino" },
 }
 
-lspconfig.jinja_lsp.setup {
-  capabilities = capabilities,
-}
 
 -- local configs = require("lspconfig.configs")
 -- local util = require("lspconfig.util")
